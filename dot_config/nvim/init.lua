33a513ca33505ebbd7ce9fc0f268c3c7d2c792c2
Lochie <itjustdirt@gmail.com>
2024-08-13 21:56:00 +1000

    bigger kitty font, changed to lualine nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -81,6 +81,7 @@ vim.opt.termguicolors = true
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
@@ -289,6 +290,7 @@ require("lazy").setup({
 	},
 	"habamax/vim-godot",
 	"preservim/vim-pencil",
+	"lambdalisue/vim-suda",
 	-- nvim v0.8.0
 	{
 		"kdheepak/lazygit.nvim",
@@ -965,23 +967,73 @@ require("lazy").setup({
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			--require('mini.surround').setup()
 
-			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
-			local statusline = require("mini.statusline")
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-			-- You can configure sections in the statusline by overriding their
-			-- default behavior. For example, here we set the section for
-			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
+			--			local statusline = require("mini.statusline")
+			--			statusline.setup({ use_icons = vim.g.have_nerd_font })
+			--
+			--			-- You can configure sections in the statusline by overriding their
+			--			-- default behavior. For example, here we set the section for
+			--			-- cursor location to LINE:COLUMN
+			--			---@diagnostic disable-next-line: duplicate-set-field
+			--			statusline.section_location = function()
+			--				return "%2l:%-2v"
+			--			end
 
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
+				},
+				sections = {
+					lualine_a = {
+						"mode",
+					},
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = {
+						{
+							"datetime",
+							-- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+							style = "%I:%M %p",
+						},
+					},
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {},
+			})
 		end,
 	},
 	{ -- Highlight, edit, and navigate code
