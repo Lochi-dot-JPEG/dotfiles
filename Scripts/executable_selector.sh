72aa59be557c54371050ac690e2c83e06a115618
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2025-05-23 14:52:48 +1000

    simplify tmux selector

--- Scripts/executable_selector.sh
+++ Scripts/executable_selector.sh
@@ -2,23 +2,9 @@
 SELECTION=$(tmuxp ls | gum filter --limit 1 --placeholder 'Pick a sesh' --prompt='>')
 
 if [[ $SELECTION != '' ]]; then
-	if [[ -n $TMUX ]]; then
-		TTY=$(tty)
-		(
-			sleep 0.3
-			if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-				tmux attach-session -t "$SESSION_NAME"
-			else
-				tmuxp load $SELECTION;
-			fi
-		) < "$TTY" > "$TTY" 2>&1 &
-
-		tmux detach
+	if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+		tmux attach-session -t "$SESSION_NAME"
 	else
-		if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-			tmux attach-session -t "$SESSION_NAME"
-		else
-			tmuxp load $SELECTION;
-		fi
+		tmuxp load $SELECTION;
 	fi
 fi
