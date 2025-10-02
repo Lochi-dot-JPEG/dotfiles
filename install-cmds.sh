c120376b3740509fa605770d67a2253d500dfba2
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2025-10-02 16:20:55 +1000

    emojis

--- install-cmds.sh
+++ install-cmds.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
-yay -Sy aseprite betterdiscord-installer-bin activitywatch-bin pnmixer ttf-ms-fonts mpdris2
-pacman -Sy neovim awesome ttf-jetbrains-mono ttf-nerd-fonts-symbols obsidian chezmoi lazygit base-devel lxappearance feh flameshot xfce4-power-manager variety swappy slurp hyprland gimagereader-qt cliphist swaync termdown
+yay -Sy aseprite \
+betterdiscord-installer-bin \
+activitywatch-bin \
+pnmixer \
+ttf-ms-fonts \
+mpdris2
+pacman -Sy neovim \
+awesome \
+ttf-jetbrains-mono \
+ttf-nerd-fonts-symbols \
+obsidian \
+chezmoi \
+lazygit \
+base-devel \
+lxappearance \
+feh \
+flameshot \
+xfce4-power-manager \
+variety \
+swappy \
+slurp \
+hyprland \
+cliphist \
+swaync \
+termdown \
+wtype
 
 mkdir ~/Pictures/Screenshots
 
@@ -11,7 +35,7 @@ git clone https://github.com/Lochi-dot-JPEG/Wallpapers
 
 # install pywal16 to .local/bin
 # ctrl + S, I in tmux to install plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 systemctl --user enable --now mpDris2.service
 # enable mpDris2 for showing mpd in waybar
